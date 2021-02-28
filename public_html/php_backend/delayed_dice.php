38f987c0e497976a975234ff4b8078a62860eaa6
Girts <girts.strazdins@gmail.com>
2021-02-28 23:24:07 +0100

    Dice now supports multi-dice mode with POST. Some refactor in Dice code

--- public_html/php_backend/delayed_dice.php
+++ public_html/php_backend/delayed_dice.php
@@ -1,9 +1,7 @@
 <?php
-require("cors_handling.php");
+require_once("cors_handling.php");
 
 // A script that returns a random number 1-6, simulates a throw of a dice.
 // It also simulates a long operation - the response is sent back after some seconds
-// Sleep time is the same as the value of the dice
-$random_number = rand(1, 6);
-sleep($random_number);
-echo $random_number;
+$sleep = true;
+require_once("throw_a_dice.php");
