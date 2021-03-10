80954c63bb7de7ac79dd2501e4c5c5bb98c522fc
Girts <girts.strazdins@gmail.com>
2021-03-10 01:08:12 +0100

    Exercise 16 added

---
+++ public_html/javascript/solutions/app-ex16.js
@@ -0,0 +1,15 @@
+// Solution for exercise 16
+
+// 1
+var person = {firstname: "Chuck", lastname: "McDuck"};
+
+// 2
+person.printName = function () {
+    console.log(this.firstname + " " + this.lastname);
+}
+
+// 3
+var student = {firstname: "Ole", lastname: "Nordmann" };
+
+// 4
+person.printName.apply(student);
