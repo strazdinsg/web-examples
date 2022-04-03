eab227bd57fb4598cb2d2cbd53019093b7e6c86d
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-03 19:09:52 +0200

    Exercise 12.1 - JS Execution context

---
+++ public_html/exercise-solutions/ex12.1-execution-context/app-ex12.js
@@ -0,0 +1,34 @@
+// Solution for exercise 12
+printG();
+
+var g = 3;
+var x = 12;
+
+printG();
+
+function printG() {
+    console.log("The value of g is " + g);
+}
+
+function a() {
+    console.log("inside a: x = " + x);
+    var y = 15;
+    console.log("y = " + y);
+}
+
+function b() {
+    console.log("inside b: x = " + x);
+    var z = 677;
+    console.log("inside b: z = " + z);
+    function c() {
+        console.log("inside c: z = " + z);
+    }
+    c();
+}
+
+console.log("globally: x = " + x);
+
+a();
+b();
+
+console.log("window.x = " + window.x);
