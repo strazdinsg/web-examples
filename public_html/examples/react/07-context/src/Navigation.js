b9bbaa81761c176e4f09d46a6e5102357a173062
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-16 22:21:37 +0200

    Example 07 - using React Context

---
+++ public_html/examples/react/07-context/src/Navigation.js
@@ -0,0 +1,24 @@
+import "./Navigation.css"
+import {useContext} from "react";
+import {ProductContext} from "./ProductContext";
+
+/**
+ * Represents navigation.
+ * No real navigation yet, just placeholders.
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export function Navigation() {
+    const products = useContext(ProductContext);
+    const productCountHint = products.length > 0 ? `(${products.length})` : "";
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li className="selected">Products {productCountHint}</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
+    );
+}
---
+++ public_html/examples/react/07-context/src/ProductGrid.js
@@ -0,0 +1,23 @@
+import {ProductCard} from "./ProductCard";
+import "./ProductGrid.css"
+import {useContext} from "react";
+import {ProductContext} from "./ProductContext";
+
+/**
+ * Component representing the product grid
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function ProductGrid() {
+    const products = useContext(ProductContext);
+
+    if (products.length > 0) {
+        return <div className="product-container">
+            {products.map(
+                product => <ProductCard product={product} key={product.id}/>
+            )}
+        </div>;
+    } else {
+        return <p className="loading">Loading products...</p>;
+    }
+}
