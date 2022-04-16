0f05a3a4a72919876b960e7365120c767c4fcbd6
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-16 22:48:26 +0200

    Example 08 - updating context from a nested component

---
+++ public_html/examples/react/08-nested-context-update/src/MainSection.js
@@ -0,0 +1,19 @@
+import {useContext} from "react";
+import {ProductContext} from "./ProductContext";
+import {ProductGrid} from "./ProductGrid";
+
+/**
+ * Component representing the main section
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function MainSection() {
+    const productContext = useContext(ProductContext);
+    const products = productContext.products;
+
+    if (products.length > 0) {
+        return <ProductGrid />;
+    } else {
+        return <p className="loading">Loading products...</p>;
+    }
+}
---
+++ public_html/examples/react/08-nested-context-update/src/ProductDeleteButton.js
@@ -0,0 +1,16 @@
+import "./ProductDeleteButton.css"
+import {useContext} from "react";
+import {ProductContext} from "./ProductContext";
+
+/**
+ * Represents the component for deleting the product
+ * @param props Properties, containing the product id
+ * @return {JSX.Element}
+ * @constructor
+ */
+export function ProductDeleteButton(props) {
+    const productContext = useContext(ProductContext);
+    const deleteFunction = productContext.handleDelete;
+
+    return <button className="product-delete" onClick={() => deleteFunction(props.id)}>Delete</button>;
+}
