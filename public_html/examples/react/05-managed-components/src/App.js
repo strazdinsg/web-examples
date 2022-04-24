260505de30930c9b23c2817fcfcbc25ad340a72a
Girts Strazdins <girts.strazdins@gmail.com>
2022-04-24 18:26:24 +0200

    Two input fields in the managed-component example

--- public_html/examples/react/05-managed-components/src/App.js
+++ public_html/examples/react/05-managed-components/src/App.js
@@ -4,28 +4,53 @@ import {useState} from "react";
  * A component representing the whole application
  */
 export function App() {
+    // Default values
     const DEFAULT_NAME = "Chuck";
+    const DEFAULT_EMAIL = "microsoft@com.chuck";
+
+    // State variables
     const [name, setName] = useState(DEFAULT_NAME);
+    const [email, setEmail] = useState(DEFAULT_EMAIL);
 
     return (
         <main>
             <h1>Managed component example</h1>
             <p>This application shows how a React component can synchronize state with an HTML element. Try to update
-                the name in the input field.</p>
+                the name or email in the input fields.</p>
             <h2>Hi, {name}!</h2>
             <label>
                 Write your name here:
-                <input type="text" value={name} onChange={handleInputChange}/>
+                <input type="text" value={name} onChange={handleNameInputChange}/>
             </label>
-            <button onClick={resetName}>Reset</button>
+            <label>
+                Write your email here:
+                <input type="text" value={email} onChange={handleEmailInputChange}/>
+            </label>
+            <button onClick={resetFields}>Reset</button>
+
+            <p>Just to see that the component gets updated DOM-element values immediately, here is your email which
+                comes from the <code>email</code> property: {email}</p>
         </main>
     );
 
-    function handleInputChange(event) {
+    /**
+     * This function is called when the value in the name input-field (DOM element) is updated
+     * @param event The DOM update event. The new value of the input field is in event.target.value
+     */
+    function handleNameInputChange(event) {
         setName(event.target.value);
     }
 
-    function resetName() {
+    /**
+     * This function is called when the value in the email input-field (DOM element) is updated
+     * @param event The DOM update event. The new value of the input field is in event.target.value
+     */
+    function handleEmailInputChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function resetFields() {
         setName(DEFAULT_NAME);
+        setEmail(DEFAULT_EMAIL);
     }
 }
