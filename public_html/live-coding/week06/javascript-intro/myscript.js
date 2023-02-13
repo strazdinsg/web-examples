21ec0c2d158557502c41536057fe4ab87f4f9b85
Girts <girts.strazdins@gmail.com>
2023-02-13 08:08:43 +0100

    Live coding from 6.x extra lecture: Javascript intro

---
+++ public_html/live-coding/week06/javascript-intro/myscript.js
@@ -0,0 +1,34 @@
+let cssHeading = document.getElementById("css-exercise-heading");
+cssHeading.addEventListener("click", handleButtonClick);
+
+cssHeading.classList.add("yellow-bg");
+
+let article = document.getElementById("css-exercises");
+
+
+function handleButtonClick() {
+  console.log("Clicked!");
+}
+
+let intervalFunc = setInterval(periodicHandler, 3000);
+
+console.log("Counter is " + counter);
+
+var counter = 0;
+
+function periodicHandler() {
+
+  console.log(`Woke up again ${++counter}`);
+  if (counter === 3) {
+    clearInterval(intervalFunc);
+    cssHeading.remove();
+    let newParagraph = document.createElement("p");
+    newParagraph.innerText = "This is a new paragraph!";
+    article.appendChild(newParagraph);
+  }
+
+}
+
+function bigLoop() {
+  console.log(d);
+}
