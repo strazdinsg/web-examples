834b3ce5a6a2a0c8835aba6c70b3db76d30ccc83
Girts <girts.strazdins@gmail.com>
2023-02-27 13:41:29 +0100

    Make the CSS stylesheet paths more generic for PHP example

--- public_html/examples/22-php-include-example/components/stylesheets.php
+++ public_html/examples/22-php-include-example/components/stylesheets.php
@@ -1,3 +1,3 @@
-<link rel="stylesheet" href="styles/main.css">
-<link rel="stylesheet" href="styles/layout.css">
-<link rel="stylesheet" href="styles/theme.css">
+<link rel="stylesheet" href="/styles/main.css">
+<link rel="stylesheet" href="/styles/layout.css">
+<link rel="stylesheet" href="/styles/theme.css">
