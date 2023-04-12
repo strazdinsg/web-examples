1f5684cc5b9e37220129c28dd0862e682bcd2bf9
Girts Strazdins <girts.strazdins@gmail.com>
2023-04-12 11:10:37 +0200

    Forgot to stage one file

--- public_html/examples/react/13-rest-api-request/src/tools/requests.js
+++ public_html/examples/react/13-rest-api-request/src/tools/requests.js
@@ -1,67 +1,54 @@
 // All code for sending requests to backend is stored in this file
-// The code is copied (and modified) from app-dev/security-examples/07-backend-frontend-jwt-auth
+// Note - no JWT token handling here!
 
-import { getCookie } from "./cookies";
+import { HttpResponseError } from "./HttpResponseError";
 
 // Import REST API BASE URL from the environment variable, see .env file
 // Note: all environment variables must start with REACT_, otherwise React will not handle them!
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
-// Note - this is an abstraction layer for HTTP requests. You can implement this function in any way
-// you want - with Axios, Fetch, ...
+// Note - this is an abstraction layer for HTTP requests.
+// You can implement this function in any way you want - with Axios,
+// XmlHttpRequest, ...
 
 /**
- * Send a REST-API request to the backend
- * @param method The method to use: GET, POST, PUT, DELETE
- * @param url relative URL of the API endpoint
- * @param callback Callback function to call on success, with response data (JSON-decoded) as the parameter
- * @param requestBody When supplied, send this data in the request body. Does not work with HTTP GET!
- * @param errorCallback A function called when the response code is not 200. Two parameters will be passed
- * to the function: HTTP response code and response body (as text)
+ * Send and asynchronous request to the remote API.
+ *
+ * @param {string} method the HTTP method to use: GET, POST, PUT. Case-insensitive.
+ * @param {string} url The relative API url (base URL is added automatically)
+ * @param {object} requestBody The data to send in request body. Ignored for HTTP GET.
+ * @return @return {Promise<JSON>} The response body, parsed as a JSON
+ * @throws {HttpResponseError} Error code and message from the response body
  */
-export function sendApiRequest(
-  method,
-  url,
-  callback,
-  requestBody,
-  errorCallback
-) {
-  const request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (request.readyState === XMLHttpRequest.DONE) {
-      if (request.status === 200) {
-        let responseJson = "";
-        if (request.responseText) {
-          responseJson = JSON.parse(request.responseText);
-        }
-        callback(responseJson);
-      } else if (errorCallback) {
-        errorCallback(request.status, request.responseText);
-      } else {
-        console.error("Error in API request");
-      }
-    }
-  };
+export function asyncApiRequest(method, url, requestBody) {
   const fullUrl = API_BASE_URL + url;
-  console.log("Sending request to " + fullUrl);
-  request.open(method, fullUrl);
-
-  // Set JWT token, if it is stored in a cookie
-  const jwtToken = getCookie("jwt");
-  if (jwtToken) {
-    request.setRequestHeader("Authorization", "Bearer " + jwtToken);
+  let body = null;
+  let headers = {};
+  if (method.toLowerCase() !== "get" && requestBody) {
+    headers["Content-Type"] = "application/json";
+    body = JSON.stringify(requestBody);
   }
 
-  // Do we need to include data in the request?
-  if (requestBody) {
-    if (method.toLowerCase() !== "get") {
-      request.setRequestHeader("Content-Type", "application/json");
-      request.send(JSON.stringify(requestBody));
-    } else {
-      console.error("Trying to send request data with HTTP GET, not allowed!");
-      request.send();
-    }
-  } else {
-    request.send();
+  return fetch(fullUrl, {
+    method: method,
+    mode: "cors",
+    headers: headers,
+    body: body,
+  })
+    .then(handleErrors)
+    .then((response) => response.json());
+}
+/**
+ * Check whether the HTTP response has a 200 OK status. If it does, return the
+ * response. If it does not, throw an Error
+ * @param response
+ * @return {response} The response (if all was OK)
+ * @throws Error containing the response code and text from the response body
+ */
+async function handleErrors(response) {
+  if (!response.ok) {
+    const responseText = await response.text();
+    throw new HttpResponseError(response.status, responseText);
   }
+  return response;
 }
